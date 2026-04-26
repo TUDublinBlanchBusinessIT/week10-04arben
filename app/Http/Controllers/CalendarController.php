48f60b4e943645a76e84c55661d07d2704df9162
04arben <b00168185@mytudublin.ie>
2026-04-26 13:32:57 +0100

    Completed PDF 3 JSON events from database

--- app/Http/Controllers/CalendarController.php
+++ app/Http/Controllers/CalendarController.php
@@ -3,11 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Event;
 
 class CalendarController extends Controller
 {
     public function display()
     {
         return view('calendar.display');
+    }
+
+    public function json()
+    {
+        return response(Event::all(), 200)
+            ->header('Content-Type', 'application/json');
     }
 }
--- resources/views/calendar/display.blade.php
+++ resources/views/calendar/display.blade.php
@@ -10,17 +10,17 @@ document.addEventListener('DOMContentLoaded', function () {
     var calendar = new FullCalendar(calendarEl, {
         plugins: [dayGridPlugin, timeGridPlugin, listPlugin, interactionPlugin],
         initialView: 'dayGridMonth',
+
         headerToolbar: {
             left: 'prev,next today',
             center: 'title',
             right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
         },
+
         slotDuration: '00:10:00',
         editable: true,
-        events: [
-            { title: 'All Day Event', start: '2023-02-20' },
-            { title: 'Long Event', start: '2023-02-22', end: '2023-02-23' }
-        ]
+        initialDate: '2026-04-28',
+        events: "{{ route('calendar.json') }}"
     });
 
     calendar.render();
