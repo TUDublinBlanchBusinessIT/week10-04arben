42ee350b86fb878a9632866781a11f81ce83d08b
04arben <b00168185@mytudublin.ie>
2026-04-26 13:08:38 +0100

    Completed PDF 2

---
+++ resources/views/calendar/display.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+@section('content')
+<div id="calendar"></div>
+
+<script>
+document.addEventListener('DOMContentLoaded', function () {
+    var calendarEl = document.getElementById('calendar');
+
+    var calendar = new FullCalendar(calendarEl, {
+        plugins: [dayGridPlugin, timeGridPlugin, listPlugin, interactionPlugin],
+        initialView: 'dayGridMonth',
+        headerToolbar: {
+            left: 'prev,next today',
+            center: 'title',
+            right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+        },
+        slotDuration: '00:10:00',
+        editable: true,
+        events: [
+            { title: 'All Day Event', start: '2023-02-20' },
+            { title: 'Long Event', start: '2023-02-22', end: '2023-02-23' }
+        ]
+    });
+
+    calendar.render();
+});
+</script>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+use App\Http\Controllers\CalendarController;
+
+Route::get('/calendar/display', [CalendarController::class, 'display'])->name('calendar.display');
